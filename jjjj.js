5d362ab7e4f1295b9b08636d60f7fb0a493de6aa
BroccolliRevolution <37957761+BroccolliRevolution@users.noreply.github.com>
2019-01-19 18:05:18 +0100

    Update jjjj.js

--- jjjj.js
+++ jjjj.js
@@ -1,4 +1,14 @@
 var type = 4;
 if (type == 'time') {
   console.log(type)
+    if (type == 'haha') {
+        if (type == 'test') {
+            var t = 0
+            if (t == type) {
+                console.log('')
+            }
+        }
+        console.log('hehe')
+    }
+    
 }
